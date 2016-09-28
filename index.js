8d528e17c10d3efb3c45b8800a033c2751853938
nathansire <nathantech2005@gmail.com>
2016-09-27 23:24:30 -0400

    auto scroll.

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+  
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-http.listen(80, function(){
-  console.log('listening on *:80');
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
