95187538708a6e167ad35993bf53791a9c42581e
nathansire <nathantech2005@gmail.com>
2016-09-29 17:26:23 -0400

    add image and style colors.

--- index.js
+++ index.js
@@ -9,14 +9,20 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+io.sockets.on('connection', function(socket){
+  socket.on('subscribe', function(room) {
+    console.log('joining room', room);
+    socket.join(room);
+  })
+
+  socket.on('unsubscribe', function(room) {
+    console.log('leaving room', room);
+    socket.leave(room);
+  })
+
+  socket.on('send', function(data) {
+    console.log('sending message: ' + data.message);
+    io.sockets.in(data.room).emit('message', data.message);
   });
 });
 
