fdcf2d5fc47cd995e99e9199c37fc064ec26bbae
nathansire <nathantech2005@gmail.com>
2016-09-29 15:57:38 -0400

    refactor js method to handle undefined.

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+function getSearchParameters() {
+  var prmstr = window.location.search.substr(1);
+  return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+  var params = {};
+  var prmarr = prmstr.split("&");
+  for ( var i = 0; i < prmarr.length; i++) {
+    var tmparr = prmarr[i].split("=");
+    params[tmparr[0]] = tmparr[1];
+  }
+  return params;
+}
+
+console.log('chat_id:' + getSearchParameters['chat_id']);
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
+  console.log('a user connected' + getSearchParameters['chat_id']);
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
